c771af61ab96012ac653dd0cab9d54fea7922972
Meet0298 <meetshah2698@gmail.com>
2022-04-08 22:08:28 +0530

    Further changes to api and started some integration of frontend and backend

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,40 +1,33 @@
 import "./post.css"
+import { Link } from "react-router-dom";
 
-export default function Post() {
+export default function Post({ post }) {
     return (
         <div className="post">
-            <img
-                className="postImg"
-                src="https://images.unsplash.com/photo-1454372182658-c712e4c5a1db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80"
-                alt=""
-            />
+            {post.photo && (
+                <img
+                    className="postImg"
+                    src={post.photo}
+                    alt=""
+                />
+            )}
             <div className="postInfo">
-                <div className="postCats">
-                    <span className="postCat">Music</span>
-                    <span className="postCat">Life</span>
+                <div className="postCats">{
+                    post.categories.map((c) => (
+                        <span className="postCat">{c.name}</span>
+                    ))
+                }
                 </div>
-                <span className="postTitle">
-                    Lorem ipsum dolor sit amet
-                </span>
+                <Link to={`/post/${post._id}`} className="link">
+                    <span className="postTitle">
+                        {post.title}
+                    </span>
+                </Link>
                 <hr />
-                <span className="postDate">1 hour ago</span>
+                <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
             </div>
             <p className="postDesc">
-                Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Quod delectus sapiente
-                distinctio aliquam at quas iusto
-                exercitationem sed! Quae nesciunt delectus officiis itaque. Delectus, quis
-                similique eius ullam reiciendis ducimus.
-                Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Quod delectus sapiente
-                distinctio aliquam at quas iusto
-                exercitationem sed! Quae nesciunt delectus officiis itaque. Delectus, quis
-                similique eius ullam reiciendis ducimus.
-                Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Quod delectus sapiente
-                distinctio aliquam at quas iusto
-                exercitationem sed! Quae nesciunt delectus officiis itaque. Delectus, quis
-                similique eius ullam reiciendis ducimus.
+                {post.desc}
             </p>
         </div>
     );
--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,45 +1,44 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom"
 import "./singlePost.css"
+import axios from "axios";
 
 export default function SinglePost() {
+    const location = useLocation();
+    // console.log(location); pathname obtained is pathname: "/post/625008204cfc550d805c7759". we need the post id. 
+    // console.log(location.pathname.split("/")[2]);
+    const path = location.pathname.split("/")[2];
+    const [post, setPost] = useState({});
+
+    useEffect(() => {
+        const getPost = async () => {
+            const res = await axios.get("/posts/" + path);
+            setPost(res.data);
+        }
+        getPost();
+    }, [path]) //Whenever this path changes, fire useEffect
     return (
         <div className="singlePost">
             <div className="singlePostWrapper">
-                <img className="singlePostImg"
-                    src="https://images.unsplash.com/photo-1454372182658-c712e4c5a1db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80"
-                    alt=""
-                />
+                {post.photo && (
+                    <img className="singlePostImg"
+                        src={post.photo}
+                        alt=""
+                    />
+                )}
                 <h1 className="singlePostTitle">
-                    Lorem ipsum dolor sit amet
+                    {post.title}
                     <div className="singlePostEdit">
                         <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
                         <i className="singlePostIcon fa-solid fa-trash-can"></i>
                     </div>
                 </h1>
                 <div className="singlePostInfo">
-                    <span className="singlePostAuthor">Author: <b>Meet</b></span>
-                    <span className="singlePostDate">1 hour ago</span>
+                    <span className="singlePostAuthor">Author: <b>{post.username}</b></span>
+                    <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
                 </div>
                 <p className="singlePostDesc">
-                    Lorem ipsum dolor sit amet consectetur,
-                    adipisicing elit. Voluptas quidem,
-                    corporis eum reiciendis nisi aperiam ullam inventore asperiores excepturi harum unde commodi cupiditate culpa nam,
-                    repellat voluptate nulla veniam ad!
-                    Lorem ipsum dolor sit amet consectetur,
-                    adipisicing elit. Voluptas quidem,
-                    corporis eum reiciendis nisi aperiam ullam inventore asperiores excepturi harum unde commodi cupiditate culpa nam,
-                    repellat voluptate nulla veniam ad!
-                    Lorem ipsum dolor sit amet consectetur,
-                    adipisicing elit. Voluptas quidem,
-                    corporis eum reiciendis nisi aperiam ullam inventore asperiores excepturi harum unde commodi cupiditate culpa nam,
-                    repellat voluptate nulla veniam ad!
-                    Lorem ipsum dolor sit amet consectetur,
-                    adipisicing elit. Voluptas quidem,
-                    corporis eum reiciendis nisi aperiam ullam inventore asperiores excepturi harum unde commodi cupiditate culpa nam,
-                    repellat voluptate nulla veniam ad!
-                    Lorem ipsum dolor sit amet consectetur,
-                    adipisicing elit. Voluptas quidem,
-                    corporis eum reiciendis nisi aperiam ullam inventore asperiores excepturi harum unde commodi cupiditate culpa nam,
-                    repellat voluptate nulla veniam ad!
+                    {post.desc}
                 </p>
             </div>
         </div>
