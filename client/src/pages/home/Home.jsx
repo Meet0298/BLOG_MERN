36509e12812c433e867c45fcf639c9f6e1bff53f
Meet0298 <meetshah2698@gmail.com>
2022-04-09 18:05:42 +0530

    Integrating frontend with backend

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,23 +4,25 @@ import Posts from "../../components/posts/Posts"
 import Sidebar from "../../components/sidebar/Sidebar"
 import "./home.css"
 import axios from "axios"
+import { useLocation } from "react-router-dom"
 
 
 export default function Home() {
     const [posts, setPosts] = useState([]);
+    const { search } = useLocation();
 
     useEffect(() => {
         const fetchPosts = async () => {
-            const res = await axios.get("/posts")
+            const res = await axios.get("/posts" + search)
             setPosts(res.data);
         }
         fetchPosts();
-    }, []) //Here empty array indicates that fire this UseEffect just at the beginning
+    }, [search])
     return (
         <>
             <Header />
             <div className="home">
-                <Posts posts={posts}/>
+                <Posts posts={posts} />
                 <Sidebar />
             </div>
         </>
