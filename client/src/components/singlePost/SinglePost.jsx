fd2c0e987705f36279b10ee4afffee0d8b62e2f7
Meet0298 <meetshah2698@gmail.com>
2022-04-11 01:49:05 +0530

    Post page and Profile icon's work done

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -12,11 +12,16 @@ export default function SinglePost() {
     const [post, setPost] = useState({});
     const PF = "http://localhost:5000/images/";
     const { user } = useContext(Context);
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+    const [updateMode, setUpdateMode] = useState(false);
 
     useEffect(() => {
         const getPost = async () => {
             const res = await axios.get("/posts/" + path);
             setPost(res.data);
+            setTitle(res.data.title);
+            setDesc(res.data.desc);
         }
         getPost();
     }, [path]); //Whenever this path changes, fire useEffect
@@ -30,6 +35,20 @@ export default function SinglePost() {
         } catch (err) {
             console.log(err);
         }
+    };
+
+    const handleUpdate = async () => {
+        try {
+            await axios.put(`/posts/${post._id}`, {
+                username: user.username,
+                title, // can also write title:title, desc:desc
+                desc,
+            });
+            // window.location.reload(); -->works, but reloads page
+            setUpdateMode(false);
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     return (
@@ -40,16 +59,29 @@ export default function SinglePost() {
                         src={PF + post.photo}
                         alt=""
                     />
-                )}
-                <h1 className="singlePostTitle">
-                    {post.title}
-                    {post.username === user?.username &&
-                        <div className="singlePostEdit">
-                            <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
-                            <i className="singlePostIcon fa-solid fa-trash-can" onClick={handleDelete}></i>
-                        </div>
-                    }
-                </h1>
+                )}{
+                    updateMode ? (
+                        <input
+                            type="text"
+                            value={title}
+                            className="singlePostTitleInput"
+                            autoFocus
+                            onChange={(e) => setTitle(e.target.value)}
+                        />) : (
+
+                        <h1 className="singlePostTitle">
+                            {title}
+                            {post.username === user?.username && (
+
+                                <div className="singlePostEdit">
+                                    <i className="singlePostIcon fa-solid fa-pen-to-square" onClick={() => setUpdateMode(true)}></i>
+                                    <i className="singlePostIcon fa-solid fa-trash-can" onClick={handleDelete}></i>
+                                </div>
+                            )
+                            }
+                        </h1>
+                    )
+                }
                 <div className="singlePostInfo">
                     <span className="singlePostAuthor">
                         Author:
@@ -59,9 +91,16 @@ export default function SinglePost() {
                     </span>
                     <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
                 </div>
-                <p className="singlePostDesc">
-                    {post.desc}
-                </p>
+                {updateMode ? (
+                    <textarea className="singlePostDescInput" value={desc} onChange={(e) => setDesc(e.target.value)} />
+                ) : (
+                    <p className="singlePostDesc">
+                        {desc}
+                    </p>
+                )}
+                {updateMode && (
+                    <button className="singlePostButton" onClick={handleUpdate}>Update</button>
+                )}
             </div>
         </div>
     )
